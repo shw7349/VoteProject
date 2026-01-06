27ba44cee1fc5adfaa0224853b6763b0f588d354
shw7349 <shw7349@naver.com>
2026-01-07 00:56:15 +0900

    init

---
+++ src/main/kotlin/com/voteproject/VoteProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.voteproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VoteProjectApplication
+
+fun main(args: Array<String>) {
+        runApplication<VoteProjectApplication>(*args)
+        }
